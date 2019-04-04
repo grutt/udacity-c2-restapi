c0726aaee888a759265fd66bc84db2235b6f00af
Gabe Ruttner <gabriel.ruttner@gmail.com>
2019-04-04 06:56:59 -0700

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,9 +1,9 @@
 export const config = {
   "dev": {
-    "username": "udagramruttnerdev",
-    "password": "udagramruttnerdev",
-    "database": "udagramruttnerdev",
-    "host": "udagramruttnerdev.c79fzt27bzf6.us-east-2.rds.amazonaws.com",
+    "username": "",
+    "password": "",
+    "database": "",
+    "host": "",
     "dialect": "postgres",
     "aws_reigion": "us-east-2",
     "aws_profile": "default",
