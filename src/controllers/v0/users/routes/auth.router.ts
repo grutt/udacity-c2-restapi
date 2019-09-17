447f197682a525456e76c1f5daed60e7891154bb
Gabe Ruttner <gabriel.ruttner@gmail.com>
2019-09-16 20:03:57 -0400

    Update auth.router.ts
    Remove starter code for comparePasswords and generatePasswords

--- src/controllers/v0/users/routes/auth.router.ts
+++ src/controllers/v0/users/routes/auth.router.ts
@@ -11,13 +11,11 @@ import * as EmailValidator from 'email-validator';
 const router: Router = Router();
 
 async function generatePassword(plainTextPassword: string): Promise<string> {
-    const saltRounds = 10;
-    let salt = await bcrypt.genSalt(saltRounds);
-    return await bcrypt.hash(plainTextPassword, salt);
+    //@TODO Use Bcrypt to Generate Salted Hashed Passwords
 }
 
 async function comparePasswords(plainTextPassword: string, hash: string): Promise<boolean> {
-    return await bcrypt.compare(plainTextPassword, hash);
+    //@TODO Use Bcrypt to Compare your password to your Salted, Hashed Password
 }
 
 function generateJWT(user: User): string {
